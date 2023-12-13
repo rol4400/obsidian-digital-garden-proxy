11d0271ae2e24a40d7d18f11beb30df4497f8fc3
Ryan Olsen <rol4400@outlook.com>
2023-12-13 16:30:58 +1000

    Redirect & changing of the serving of error pages

--- netlify-functions/GetPageContent.js
+++ netlify-functions/GetPageContent.js
@@ -1,5 +1,7 @@
 // getPageContent.js
-const { Deta } = require('deta');
+const {
+    Deta
+} = require('deta');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
@@ -7,124 +9,155 @@ const deta = Deta(process.env.DETA_PROJECT_KEY);
 const linksTable = deta.Base('Obsidian_Links');
 
 exports.handler = async (req, context) => {
-  try {
+    try {
 
-    const { token } = req.queryStringParameters;
+        const {
+            token
+        } = req.queryStringParameters;
 
-    // Check if the token exists
-    if (!token) {
-      return {
-        statusCode: 403,
-        body: await getErrorPage(403, "The link given was invalid, please ask for another"),
-    };
+        // Check if the token exists
+        if (!token) {
+            return {
+                statusCode: 302,
+                headers: {
+                    'Location': `${process.env.BASE_URL}/403.html`,
+                },
+                body: '',
+            };
+        }
+
+        // Retrieve link information from Deta.Base
+        const linkInfo = await linksTable.get(token);
+
+        // Check if the link has expired
+        if (!linkInfo || Date.now() > linkInfo.expirationTime) {
+            return {
+                statusCode: 302,
+                headers: {
+                    'Location': `${process.env.BASE_URL}/403.html`,
+                },
+                body: '',
+            };
+        }
+
+        // Make an API call to get the page content
+        const response = await axios.get(linkInfo.address);
+
+        // Update links and extract head and body sections
+        const {
+            head,
+            body
+        } = updateAssetUrls(response.data, linkInfo.address, "token");
+
+        // Return the modified response
+        return {
+            statusCode: response.status,
+            headers: {
+                'Content-Type': 'text/html',
+            },
+            body: `<html>${head}<body>${body}</body></html>`,
+        };
+    } catch (error) {
+        console.error('Error:', error);
+
+        // Customize error response based on the error type
+        let statusCode = 500;
+
+        if (error.response && error.response.status) {
+            statusCode = error.response.status;
+        }
+
+        // Return the error response with redirect to the custom error page
+        return {
+            statusCode: 500,
+            headers: {
+                'Location': `${process.env.BASE_URL}/${statusCode}.html`,
+            },
+            body: '',
+        };
     }
-
-    // Retrieve link information from Deta.Base
-    const linkInfo = await linksTable.get(token);
-
-    // Check if the link has expired
-    if (!linkInfo || Date.now() > linkInfo.expirationTime) {
-      return {
-        statusCode: 403,
-        body: await getErrorPage(403, "The link given was invalid, please ask for another"),
-    };
-    }
-
-
-    // Make an API call to get the page content
-    const response = await axios.get(linkInfo.address);
-
-    // Update links and extract head and body sections
-    const { head, body }  = updateAssetUrls(response.data, linkInfo.address, "token");
-
-    // Return the modified response
-    return {
-      statusCode: response.status,
-      headers: {
-        'Content-Type': 'text/html',
-      },
-      body: `<html>${head}<body>${body}</body></html>`,
-    };
-  } catch (error) {
-    console.error('Error:', error);
-    return {
-      statusCode: 500,
-      body: await getErrorPage(500, "Internal Server Error"),
-    };
-  }
 };
 
 // Update the extractHeadAndBody function in getPageContent.js
 function extractHeadAndBody(htmlContent) {
-  // Find the first occurrence of <head> and <body>
-  const headStartIndex = htmlContent.indexOf('<head>');
-  const headEndIndex = htmlContent.indexOf('</head>');
-  const bodyStartIndex = htmlContent.indexOf('<body>');
-  const bodyEndIndex = htmlContent.indexOf('</body>');
+    // Find the first occurrence of <head> and <body>
+    const headStartIndex = htmlContent.indexOf('<head>');
+    const headEndIndex = htmlContent.indexOf('</head>');
+    const bodyStartIndex = htmlContent.indexOf('<body>');
+    const bodyEndIndex = htmlContent.indexOf('</body>');
 
-  // If <head> and <body> are found, extract the content
-  if (headStartIndex !== -1 && headEndIndex !== -1 && bodyStartIndex !== -1 && bodyEndIndex !== -1) {
-    const head = htmlContent.substring(headStartIndex, headEndIndex + '</head>'.length);
-    const body = htmlContent.substring(bodyStartIndex + '<body>'.length, bodyEndIndex);
+    // If <head> and <body> are found, extract the content
+    if (headStartIndex !== -1 && headEndIndex !== -1 && bodyStartIndex !== -1 && bodyEndIndex !== -1) {
+        const head = htmlContent.substring(headStartIndex, headEndIndex + '</head>'.length);
+        const body = htmlContent.substring(bodyStartIndex + '<body>'.length, bodyEndIndex);
 
-    return { head, body };
-  }
+        return {
+            head,
+            body
+        };
+    }
 
-  // If not found, return the original content
-  return { head: '', body: htmlContent };
+    // If not found, return the original content
+    return {
+        head: '',
+        body: htmlContent
+    };
 }
 
 // Update the updateAssetUrls function
 function updateAssetUrls(htmlContent, originalAddress, token) {
     try {
-      const $ = cheerio.load(htmlContent);
-      const originalDomain = originalAddress.split('/').slice(0, 3).join('/');
-  
-      // Update URLs for style and script assets with the original domain
-      $('link[href], script[src]').each((index, element) => {
-        const path = $(element).attr('href') || $(element).attr('src');
-        const resolvedUrl = new URL(path, originalDomain).toString();
-        $(element).attr('href', resolvedUrl);
-        $(element).attr('src', resolvedUrl);
-      });
-  
-      // Update href links to append the token query parameter
-      $('a[href^="/"]').each((index, element) => {
-        const path = $(element).attr('href');
-        const resolvedUrl = new URL(path, originalDomain);
-        resolvedUrl.searchParams.set('token', token);
-        $(element).attr('href', resolvedUrl.toString());
-      });
-  
-      // Update inline JavaScript fetch calls
-      $('script').each((index, element) => {
-        const scriptContent = $(element).html();
-        if (scriptContent.includes('fetch')) {
-          const updatedScript = scriptContent.replace(/fetch\('\/graph.json'\)/g, `fetch('${originalDomain}/graph.json?token=${token}')`);
-          $(element).html(updatedScript);
-        }
-      });
-  
-      // Serialize the modified document back to HTML
-      const updatedContent = $.html();
-  
-      // Extract head and body sections
-      const headBodyContent = extractHeadAndBody(updatedContent);
-  
-      return headBodyContent;
+        const $ = cheerio.load(htmlContent);
+        const originalDomain = originalAddress.split('/').slice(0, 3).join('/');
+
+        // Update URLs for style and script assets with the original domain
+        $('link[href], script[src]').each((index, element) => {
+            const path = $(element).attr('href') || $(element).attr('src');
+            const resolvedUrl = new URL(path, originalDomain).toString();
+            $(element).attr('href', resolvedUrl);
+            $(element).attr('src', resolvedUrl);
+        });
+
+        // Update href links to append the token query parameter
+        $('a[href^="/"]').each((index, element) => {
+            const path = $(element).attr('href');
+            const resolvedUrl = new URL(path, originalDomain);
+            resolvedUrl.searchParams.set('token', token);
+            $(element).attr('href', resolvedUrl.toString());
+        });
+
+        // Update inline JavaScript fetch calls
+        $('script').each((index, element) => {
+            const scriptContent = $(element).html();
+            if (scriptContent.includes('fetch')) {
+                const updatedScript = scriptContent.replace(/fetch\('\/graph.json'\)/g, `fetch('${originalDomain}/graph.json?token=${token}')`);
+                $(element).html(updatedScript);
+            }
+        });
+
+        // Serialize the modified document back to HTML
+        const updatedContent = $.html();
+
+        // Extract head and body sections
+        const headBodyContent = extractHeadAndBody(updatedContent);
+
+        return headBodyContent;
     } catch (error) {
-      console.error('Error in updateAssetUrls:', error);
-      return { head: '', body: htmlContent };
+        console.error('Error in updateAssetUrls:', error);
+        return {
+            head: '',
+            body: htmlContent
+        };
     }
-  }
-  
-  // Helper function to get custom error page content
+}
+
+// Helper function to get custom error page content
 async function getErrorPage(statusCode, errorMessage) {
     try {
-      const response = await axios.get(`${process.env.BASE_URL}/${statusCode}.html`);
-      return response.data;
+        const response = await axios.get(`${process.env.BASE_URL}/${statusCode}.html`);
+        return response.data;
     } catch (error) {
-      // If the custom error page is not found, return a simple error message
-      return `<html><head><title>${errorMessage}</title></head><body><h1>${errorMessage}</h1></body></html>`;
+        // If the custom error page is not found, return a simple error message
+        return `<html><head><title>${errorMessage}</title></head><body><h1>${errorMessage}</h1></body></html>`;
     }
-  }
+}
