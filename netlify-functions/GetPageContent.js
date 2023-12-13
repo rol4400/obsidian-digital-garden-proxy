4eb0b1ee1a2bbb412e035a835ceb75e798bc7d2f
Ryan Olsen <rol4400@outlook.com>
2023-12-13 20:02:13 +1000

    Updated the toast style

--- netlify-functions/GetPageContent.js
+++ netlify-functions/GetPageContent.js
@@ -166,16 +166,100 @@ function injectWarningAlert(htmlContent, expirationTime) {
 
         // Construct the warning alert HTML
         const warningAlert = `
-            <div class="alert fade alert-simple alert-warning alert-dismissible text-left font__family-montserrat font__size-16 font__weight-light brk-library-rendered rendered show" role="alert" data-brk-library="component__alert">
-                <button type="button" class="close font__size-18" data-dismiss="alert">
-                    <span aria-hidden="true">
-                        <i class="fa fa-times warning"></i>
-                    </span>
-                    <span class="sr-only">Close</span>
-                </button>
-                <i class="start-icon fa fa-exclamation-triangle faa-flash animated"></i>
-                <strong class="font__weight-semibold">Warning!</strong> ${warningMessage}
+            <div id="toast" class="show">
+                <div id="img">Icon</div>
+                <div id="desc">${warningMessage}</div>
             </div>
+            <style>
+                #toast {
+                    visibility: hidden;
+                    max-width: 50px;
+                    height: 50px;
+                    margin: auto;
+                    background-color: #333;
+                    color: #fff;
+                    text-align: center;
+                    border-radius: 2px;
+                    position: fixed;
+                    z-index: 1;
+                    left: 0;
+                    right: 0;
+                    bottom: 30px;
+                    font-size: 17px;
+                    white-space: nowrap;
+                }
+                #toast #img {
+                    width: 50px;
+                    height: 50px;
+                    float: left;
+                    padding-top: 16px;
+                    padding-bottom: 16px;
+                    box-sizing: border-box;
+                    background-color: #111;
+                    color: #fff;
+                }
+                #toast #desc {
+                    color: #fff;
+                    padding: 16px;
+                    overflow: hidden;
+                    white-space: nowrap;
+                }
+                #toast.show {
+                    visibility: visible;
+                    -webkit-animation: fadein 0.5s, expand 0.5s 0.5s, stay 3s 1s, shrink 0.5s 2s, fadeout 0.5s 2.5s;
+                    animation: fadein 0.5s, expand 0.5s 0.5s, stay 3s 1s, shrink 0.5s 4s, fadeout 0.5s 4.5s;
+                }
+                @-webkit-keyframes fadein {
+                    from { bottom: 0; opacity: 0; }
+                    to { bottom: 30px; opacity: 1; }
+                }
+                @keyframes fadein {
+                    from { bottom: 0; opacity: 0; }
+                    to { bottom: 30px; opacity: 1; }
+                }
+                @-webkit-keyframes expand {
+                    from { min-width: 50px; }
+                    to { min-width: 350px; }
+                }
+                @keyframes expand {
+                    from { min-width: 50px; }
+                    to { min-width: 350px; }
+                }
+                @-webkit-keyframes stay {
+                    from { min-width: 350px; }
+                    to { min-width: 350px; }
+                }
+                @keyframes stay {
+                    from { min-width: 350px; }
+                    to { min-width: 350px; }
+                }
+                @-webkit-keyframes shrink {
+                    from { min-width: 350px; }
+                    to { min-width: 50px; }
+                }
+                @keyframes shrink {
+                    from { min-width: 350px; }
+                    to { min-width: 50px; }
+                }
+                @-webkit-keyframes fadeout {
+                    from { bottom: 30px; opacity: 1; }
+                    to { bottom: 60px; opacity: 0; }
+                }
+                @keyframes fadeout {
+                    from { bottom: 30px; opacity: 1; }
+                    to { bottom: 60px; opacity: 0; }
+                }
+            </style>
+            <script>
+                document.addEventListener("DOMContentLoaded", function () {
+                    function launch_toast() {
+                        var x = document.getElementById("toast");
+                        x.className = "show";
+                        setTimeout(function () { x.className = x.className.replace("show", ""); }, 5000);
+                    }
+                    launch_toast();
+                });
+            </script>
         `;
 
         // Inject the warning alert at the beginning of the body
