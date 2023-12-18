9f4e43b5b1607bfd422536d320ce669a36b6b55f
Ryan Olsen <rol4400@outlook.com>
2023-12-19 01:07:33 +1000

    Splitting userData into an array

--- netlify-functions/GetPageContent.js
+++ netlify-functions/GetPageContent.js
@@ -95,7 +95,7 @@ exports.handler = async (req, context) => {
                 .digest();
     
             // Extract 'userData' from cookies
-            const userData = sessionCookies.split(';').find(cookie => cookie.trim().startsWith('userData=')).split(/=(.*)/s)[1];
+            const userData = (sessionCookies.split(';').find(cookie => cookie.trim().startsWith('userData=')).split(/=(.*)/s)[1]).split('&');
 
             // this is the data to be authenticated i.e. telegram user id, first_name, last_name etc.
             const dataCheckString = Object.keys(userData)
