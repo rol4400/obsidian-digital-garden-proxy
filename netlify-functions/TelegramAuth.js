c16d93ae772001d28411bcf349ac39a1f7339d60
Ryan Olsen <rol4400@outlook.com>
2023-12-19 00:09:37 +1000

    Error handling for TelegramAuth.js

--- netlify-functions/TelegramAuth.js
+++ netlify-functions/TelegramAuth.js
@@ -1,29 +1,43 @@
 
 exports.handler = async (req, context) => {
-    // Extract query parameters from the request
-    const { referer, ...queryParams } = req.queryStringParameters;
+    try {
+        // Extract query parameters from the request
+        const { referer, ...queryParams } = req.queryStringParameters;
+    
+        // Create a string representation of the remaining query parameters
+        const queryString = Object.entries(queryParams)
+            .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+            .join('&');
+    
+        // Set the entire query string as a cookie
+        const cookieValue = encodeURIComponent(req.rawQueryString);
+        const setCookieHeader = `Set-Cookie: userDatay=${cookieValue}; Path=/;`;
+    
+        // Construct the redirect URL without the 'referer' parameter
+        const redirectUrl = `/${referer}?${queryString}`;
+    
+        // Return a response with the redirect and cookie headers
+        return {
+            statusCode: 302,
+            headers: {
+                Location: redirectUrl,
+                'Cache-Control': 'no-cache',
+                'Content-Type': 'text/plain',
+                'Set-Cookie': setCookieHeader,
+            },
+            body: 'Redirecting...',
+        };
+    
+    } catch (error) {
+        console.error('Error:', error);
 
-    // Create a string representation of the remaining query parameters
-    const queryString = Object.entries(queryParams)
-        .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-        .join('&');
-
-    // Set the entire query string as a cookie
-    const cookieValue = encodeURIComponent(req.rawQueryString);
-    const setCookieHeader = `Set-Cookie: userDatay=${cookieValue}; Path=/;`;
-
-    // Construct the redirect URL without the 'referer' parameter
-    const redirectUrl = `/${referer}?${queryString}`;
-
-    // Return a response with the redirect and cookie headers
-    return {
-        statusCode: 302,
-        headers: {
-            Location: redirectUrl,
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'text/plain',
-            'Set-Cookie': setCookieHeader,
-        },
-        body: 'Redirecting...',
-    };
+        // Return the error response with redirect to the custom error page
+        return {
+            statusCode: error.statusCode,
+            headers: {
+                'Location': `/500.html`,
+            },
+            body: 'Internal server error',
+        };
+    }
 };
